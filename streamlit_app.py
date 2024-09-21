4f7ca75ac6bae8d6d109765ee6f4ba193867e9ef
MustafaAKY <144323635+MustafaAKY@users.noreply.github.com>
2024-09-22 01:01:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ with tab11:
                 ico = "money.png" 
         else:
                 ico = "bmw.png"
-        st.image(ico,width=3)
+        st.image(ico,width=30)
         SUBELER = [
                 "ARAS KARGO",
                 "PTT",]
